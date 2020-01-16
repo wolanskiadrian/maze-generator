0f03e8ed32988294d223c581d7d9e0c552b24dd6
Adrian Wolański <adrian.wolanski@likims.com>
2020-01-16 10:38:49 +0100

    find available neighbors and select random one as new current

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let cols;
 let rows;
-const size = 40;
+const size = 80;
 const grid = [];
 
 let current;
@@ -17,22 +17,28 @@ function setup() {
     }
   }
 
-  current = grid[0];
+  current = grid[23];
   current.visited = true;
-
-  console.log(grid);
 }
 
 function draw() {
   background(51);
+  frameRate(1);
 
-  for(let i = 0; i < grid.length; i++) {
+  for (let i = 0; i < grid.length; i++) {
     grid[i].show();
 
-    if(grid[i].visited) {
-      grid[i].highlight();
+    if (grid[i].visited) {
+      grid[i].highlight(100);
     }
   }
+
+  const availableNeighbors = current.checkNeighbors();
+  const next = random(availableNeighbors);
+  console.log(next);
+
+  next.visited = true;
+  current = next;
 }
 
 function Cell(i, j) {
@@ -68,10 +74,57 @@ function Cell(i, j) {
     }
   };
 
-  this.highlight = () => {
-    fill(100);
+  this.highlight = (...args) => {
+    fill(...args);
     rect(x, y, size, size);
   };
+
+  this.checkNeighbors = () => {
+    let  neighbors = [];
+
+    console.log(this.i + ' ' + this.j);
+
+    console.log('top: ', getIndex(this.i, this.j - 1));
+    console.log('right: ', getIndex(this.i + 1, this.j ));
+    console.log('bottom: ', getIndex(this.i, this.j + 1));
+    console.log('left: ', getIndex(this.i - 1, this.j));
+
+    const top = grid[getIndex(this.i, this.j - 1)];
+    const right = grid[getIndex(this.i + 1, this.j )];
+    const bottom = grid[getIndex(this.i, this.j + 1)];
+    const left = grid[getIndex(this.i - 1, this.j)];
+
+    // top.highlight(66, 135, 245); // blue
+    // right.highlight(63, 224, 101); // green
+    // bottom.highlight(209, 54, 103); // red
+    // left.highlight(204, 139, 47); // orange
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+
+    return neighbors;
+  };
+}
+
+function getIndex(i, j) {
+  if (i < 0 || j < 0 || i > rows - 1 || j > cols -1 ) {
+    return -1;
+  }
+
+  return j + i * cols;
 }
 
 
